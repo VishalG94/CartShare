46ecefc3dd3185374ca6f85c25df72a6a8e87643
Vishal <vishalgadapa2468@gmail.com>
2020-04-20 21:35:59 -0700

    Starter code

---
+++ Lab2/src/main/java/edu/sjsu/cmpe275/project/CartShare/CartShareApplication.java
@@ -0,0 +1,13 @@
+package edu.sjsu.cmpe275.project.CartShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartShareApplication.class, args);
+	}
+
+}
